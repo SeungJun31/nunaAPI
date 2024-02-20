f443bd3a4c01bb7ad3d2e10a67b3aa12e14c8894
Seungjun_Lee <i44920314@gmail.com>
2024-02-21 07:13:06 +0900

    4commit

--- main.js
+++ main.js
@@ -1,18 +1,49 @@
-const API_KEY=`b69bbdaa05d2405b8c8f9e574293bcba`
+const API_KEY = `b69bbdaa05d2405b8c8f9e574293bcba`;
 let newsList = [];
-const getLatesNews = async() => {
-  url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-  //url인스턴스는 url에 필요한 함수와 변수들을 제공함 
-  const response = await fetch(url) //url 내의 데이터를 가지고 옴
+const menus = document.querySelectorAll(".menus button");
+
+menus.forEach((menu) =>
+  menu.addEventListener("click", () => getNewsByCategory(event))
+);
+
+const getLatesNews = async () => {
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  );
+  //url인스턴스는 url에 필요한 함수와 변수들을 제공함
+  const response = await fetch(url); //url 내의 데이터를 가지고 옴
   const data = await response.json(); //json의 형태로 데이터를 만들어 줌
   newsList = data.articles;
   render();
-  console.log(newsList);
 };
 getLatesNews();
 
-const render=() => {
-  const newsHTML = newsList.map((news) => `<div class="row news">
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url); 
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+};
+
+const getNewsBySearch = async () => {
+  const keyword = document.getElementById("input-search").value;
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url); 
+  const data = await response.json(); 
+  newsList = data.articles;
+  render();
+};
+
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
   <div class="col-lg-4">
     <img class="news-img-size"
     src=${news.urlToImage}>
@@ -23,6 +54,7 @@ const render=() => {
     <div>${news.source.name} * ${news.publishedAt}</div>
   </div>
 </div>`
-).join(' ') //string타입으로 바꿈으로써 배열의 ,제거
-  document.getElementById('news-board').innerHTML = newsHTML;
-}
+    )
+    .join(" "); //string타입으로 바꿈으로써 배열의 ,제거
+  document.getElementById("news-board").innerHTML = newsHTML;
+};
